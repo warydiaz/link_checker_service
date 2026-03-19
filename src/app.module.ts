3042fa27277afcbd801bc239391d6a58c4257c08
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2026-03-19 17:23:01 +0100

    feat: update package.json with description and author; restructure app module and add Jobs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { JobsModule } from './Jobs/jobs.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [JobsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
